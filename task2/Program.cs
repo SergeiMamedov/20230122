aa4f6f3fe0410a86ff5f7ce41de748c49d06636f
Sergei <sergeimamedov9976@gmail.com>
2023-01-22 15:47:01 +0600

    вторая задача

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка и показывает наибольшую цифру этого числа
+
+int a = new Random().Next(10, 100);
+System.Console.WriteLine($"Сравниваем цифры в {a}");
+int b = a / 10;
+int c = a % 10;
+if (b > c)
+{
+    System.Console.WriteLine($"Больше {b}");
+}
+else
+{
+    System.Console.WriteLine($"Больше {c}");
+}
